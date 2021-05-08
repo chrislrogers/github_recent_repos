fa290fba6340a4536bcc5089a10fc7905fecdf83
Chris Rogers <47615352+chrislrogers@users.noreply.github.com>
2021-05-08 03:16:25 +0100

    Added repo limit and added repo description
    Changed the amount of repos to ten from default.
    
    Added the description of the repo to the HTML

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Github Recent Repos
  Displays the 10 most recently updated repos of a user
- 
+
 View live [here](https://chrislrogers.github.io/github_recent_repos/)
--- script.js
+++ script.js
@@ -2,14 +2,21 @@ const RECENT_REPOS = document.getElementById("recent_repos");
 
 function showRepos(name) {
     let githubUser = name;
-    let githubApiUrl = 'https://api.github.com/users/' + githubUser + '/repos'
+    let githubApiUrl = 'https://api.github.com/users/' + githubUser + '/repos?per_page=10';
 
     fetch(githubApiUrl)
     .then((response) => response.json())
     .then((data) => {
         RECENT_REPOS.innerHTML = '';
         for (let i = 0; i < data.length; i++) {
-            RECENT_REPOS.insertAdjacentHTML('beforeend', `<h3 id="project-name">${data[i].name}</h3><a id="project-url" href="${data[i].html_url}" target="_blank">Source Code</a>`);
+            RECENT_REPOS.insertAdjacentHTML('beforeend', `<h3 id="project-name">${data[i].name}</h3>`);
+            
+            if (data[i].description !== null) {
+                RECENT_REPOS.insertAdjacentHTML('beforeend',` <p>${data[i].description}</p>`);
+            }
+
+            RECENT_REPOS.insertAdjacentHTML('beforeend',`<a id="project-url" href="${data[i].html_url}" target="_blank">Source Code</a>`);
+            
             if (data[i].homepage !== "" && data[i].homepage !== null) {
                 RECENT_REPOS.insertAdjacentHTML('beforeend',` <a id="project-live" href="${data[i].homepage}" target="_blank">Live</a>`);
             }
