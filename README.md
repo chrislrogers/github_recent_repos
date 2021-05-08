a78f734fd9da23a1d098071430adc224796657ef
Chris Rogers <47615352+chrislrogers@users.noreply.github.com>
2021-05-08 19:37:50 +0100

    Formatting changes, updated readme

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # Github Recent Repos
- Displays the 10 most recently updated repos of a user
+ Displays the 10 most recently updated repos of a user. 
+ 
+ Repo information includes:
+
+ * Repo name
+ * Time last updated
+ * Language
+ * Description
+ * Source code 
+ * Website (if provided)
 
 View live [here](https://chrislrogers.github.io/github_recent_repos/)
--- script.js
+++ script.js
@@ -5,32 +5,32 @@ function showRepos(name) {
     let githubApiUrl = 'https://api.github.com/users/' + githubUser + '/repos?per_page=10';
 
     fetch(githubApiUrl)
-    .then((response) => response.json())
-    .then((data) => {
-        RECENT_REPOS.innerHTML = '';
-        for (let i = 0; i < data.length; i++) {
-            let cardHtml = `<div class="card">`;
-            let date = new Date(data[i].updated_at);
-            let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+        .then((response) => response.json())
+        .then((data) => {
+            RECENT_REPOS.innerHTML = '<!-- Code injected by script.js -->';
+            for (let i = 0; i < data.length; i++) {
+                let cardHtml = `<div class="card">`;
+                let date = new Date(data[i].updated_at);
+                let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
-            cardHtml = cardHtml + `<h3 id="project-name">${data[i].name}</h3>`;
-            cardHtml = cardHtml + `<h5>Updated: ${date.toLocaleDateString('en-US', options)}</h5>`;
-            cardHtml = cardHtml + `<h4 id="project-language">${data[i].language}</h4>`;
+                cardHtml = cardHtml + `<h3 id="project-name">${data[i].name}</h3>`;
+                cardHtml = cardHtml + `<h5>Updated: ${date.toLocaleDateString('en-US', options)}</h5>`;
+                cardHtml = cardHtml + `<h4 id="project-language">${data[i].language}</h4>`;
 
-            if (data[i].description !== null) {
-                cardHtml = cardHtml + ` <p>${data[i].description}</p>`;
+                if (data[i].description !== null) {
+                    cardHtml = cardHtml + ` <p>${data[i].description}</p>`;
+                }
+                cardHtml = cardHtml + `<br><div class="links">`;
+                cardHtml = cardHtml + `<a id="project-url" href="${data[i].html_url}" target="_blank">Source Code</a>`;
+
+                if (data[i].homepage !== "" && data[i].homepage !== null) {
+                    cardHtml = cardHtml + ` <a id="project-live" href="${data[i].homepage}" target="_blank">Website</a>`;
+                }
+                else {
+                    cardHtml = cardHtml + `<a class="no-link" href="#">Website</a>`;
+                }
+                cardHtml = cardHtml + `</div></div>`;
+                RECENT_REPOS.insertAdjacentHTML("beforeend", cardHtml);
             }
-            cardHtml = cardHtml + `<br><div class="links">`;
-            cardHtml = cardHtml + `<a id="project-url" href="${data[i].html_url}" target="_blank">Source Code</a>`;
-            
-            if (data[i].homepage !== "" && data[i].homepage !== null) {
-                cardHtml = cardHtml + ` <a id="project-live" href="${data[i].homepage}" target="_blank">Live</a>`;
-            }
-            else {
-                cardHtml = cardHtml + `<a class="no-link" href="#">Live</a>`;
-            }
-            cardHtml = cardHtml + `</div></div>`;
-            RECENT_REPOS.insertAdjacentHTML("beforeend", cardHtml);
-        }
-    })
+        })
 }
